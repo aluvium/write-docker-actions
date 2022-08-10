f77ad79a7b7b77d295cb43450212742295997796
aluvium <aluvium@protonmail.com>
2022-08-10 12:20:32 +0000

    maingo

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -7,9 +7,9 @@ func main() {
      firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
      secondGreeting := os.Getenv("INPUT_SECONDGREETING")
      thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
-	
+     
     fmt.Println("Hello" + firstGreeting)
-    fmt.Println("Hello" + secondGreeting) 
+    fmt.Println("Hello" + secondGreeting)
     if thirdGreeting != "" {
         fmt.Println("Hello" + thirdGreeting)
     }
